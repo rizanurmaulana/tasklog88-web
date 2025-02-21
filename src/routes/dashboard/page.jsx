affaffb3d2e04d72ad2a0bc6103bbcefc841aa62
rizanurmaulana <rizanurmaulana2810@gmail.com>
2025-02-21 09:43:47 +0700

    feat: show total from database

--- src/routes/dashboard/page.jsx
+++ src/routes/dashboard/page.jsx
@@ -1,236 +1,108 @@
-import {
-  Area,
-  AreaChart,
-  ResponsiveContainer,
-  Tooltip,
-  XAxis,
-  YAxis,
-} from 'recharts';
-import { overviewData, recentSalesData, topProducts } from '../../constants';
-import {
-  CreditCard,
-  DollarSign,
-  Package,
-  PencilLine,
-  Star,
-  Trash,
-  TrendingUp,
-  Users,
-} from 'lucide-react';
+import axios from 'axios';
+import { ListCheck, NotepadText, Package, Users } from 'lucide-react';
+import { useEffect, useState } from 'react';
 
 const DashboardPage = () => {
+  const token = localStorage.getItem('token');
+  const [totalProject, setTotalProject] = useState(0);
+  const [totalTask, setTotalTask] = useState(0);
+  const [totalPengerjaan, setTotalPengerjaan] = useState(0);
+  const [totalUser, setTotalUser] = useState(0);
+
+  useEffect(() => {
+    const fetchTotalProject = async () => {
+      try {
+        const res = await axios.get('http://localhost:9000/api/v1/projects', {
+          headers: {
+            Authorization: `Bearer: ${token}`,
+          },
+        });
+        setTotalProject(res.data.project.totalData);
+      } catch (error) {
+        console.error('Error fetching total project:', error);
+      }
+    };
+
+    fetchTotalProject();
+  }, []);
+
+  useEffect(() => {
+    const fetchTotalTask = async () => {
+      const res = await axios.get('http://localhost:9000/api/v1/tasks', {
+        headers: {
+          Authorization: `Bearer: ${token}`,
+        },
+      });
+      setTotalTask(res.data.data.total);
+    };
+
+    fetchTotalTask();
+  }, []);
+
+  useEffect(() => {
+    const fetchTotalPengerjaan = async () => {
+      const res = await axios.get('http://localhost:9000/api/v1/pengerjaans', {
+        headers: {
+          Authorization: `Bearer: ${token}`,
+        },
+      });
+      setTotalPengerjaan(res.data.data.total);
+    };
+
+    fetchTotalPengerjaan();
+  }, []);
+
+  useEffect(() => {
+    const fetchTotalUser = async () => {
+      const res = await axios.get('http://localhost:9000/api/v1/users', {
+        headers: {
+          Authorization: `Bearer: ${token}`,
+        },
+      });
+      setTotalUser(res.data.project.totalData);
+    };
+
+    fetchTotalUser();
+  }, []);
+
+  const statsData = [
+    {
+      label: 'Total Project',
+      value: totalProject,
+      icon: <Package size={26} />,
+    },
+    {
+      label: 'Total Tugas',
+      value: totalTask,
+      icon: <NotepadText size={26} />,
+    },
+    {
+      label: 'Total Pengerjaan',
+      value: totalPengerjaan,
+      icon: <ListCheck size={26} />,
+    },
+    { label: 'Total User', value: totalUser, icon: <Users size={26} /> },
+  ];
+
   return (
     <div className='flex flex-col gap-y-4'>
       <h1 className='title'>Dashboard</h1>
       <div className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
-        <div className='card'>
-          <div className='card-header'>
-            <div className='w-fit rounded-lg bg-blue-500/20 p-2 text-blue-500 transition-colors'>
-              <Package size={26} />
-            </div>
-            <p className='card-title'>Total Products</p>
-          </div>
-          <div className='card-body bg-slate-100 transition-colors'>
-            <p className='text-3xl font-bold text-slate-900 transition-colors'>
-              25,154
-            </p>
-            <span className='flex w-fit items-center gap-x-2 rounded-full border border-blue-500 px-2 py-1 font-medium text-blue-500'>
-              <TrendingUp size={18} />
-              25%
-            </span>
-          </div>
-        </div>
-        <div className='card'>
-          <div className='card-header'>
-            <div className='rounded-lg bg-blue-500/20 p-2 text-blue-500 transition-colors'>
-              <DollarSign size={26} />
-            </div>
-            <p className='card-title'>Total Paid Orders</p>
-          </div>
-          <div className='card-body bg-slate-100 transition-colors'>
-            <p className='text-3xl font-bold text-slate-900 transition-colors'>
-              $16,000
-            </p>
-            <span className='flex w-fit items-center gap-x-2 rounded-full border border-blue-500 px-2 py-1 font-medium text-blue-500'>
-              <TrendingUp size={18} />
-              12%
-            </span>
-          </div>
-        </div>
-        <div className='card'>
-          <div className='card-header'>
-            <div className='rounded-lg bg-blue-500/20 p-2 text-blue-500 transition-colors'>
-              <Users size={26} />
-            </div>
-            <p className='card-title'>Total Customers</p>
-          </div>
-          <div className='card-body bg-slate-100 transition-colors'>
-            <p className='text-3xl font-bold text-slate-900 transition-colors'>
-              15,400k
-            </p>
-            <span className='flex w-fit items-center gap-x-2 rounded-full border border-blue-500 px-2 py-1 font-medium text-blue-500'>
-              <TrendingUp size={18} />
-              15%
-            </span>
-          </div>
-        </div>
-        <div className='card'>
-          <div className='card-header'>
-            <div className='rounded-lg bg-blue-500/20 p-2 text-blue-500 transition-colors'>
-              <CreditCard size={26} />
-            </div>
-            <p className='card-title'>Sales</p>
-          </div>
-          <div className='card-body bg-slate-100 transition-colors'>
-            <p className='text-3xl font-bold text-slate-900 transition-colors'>
-              12,340
-            </p>
-            <span className='flex w-fit items-center gap-x-2 rounded-full border border-blue-500 px-2 py-1 font-medium text-blue-500'>
-              <TrendingUp size={18} />
-              19%
-            </span>
-          </div>
-        </div>
-      </div>
-      <div className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-7'>
-        <div className='card col-span-1 md:col-span-2 lg:col-span-4'>
-          <div className='card-header'>
-            <p className='card-title'>Overview</p>
-          </div>
-          <div className='card-body p-0'>
-            <ResponsiveContainer width='100%' height={300}>
-              <AreaChart
-                data={overviewData}
-                margin={{
-                  top: 0,
-                  right: 0,
-                  left: 0,
-                  bottom: 0,
-                }}
-              >
-                <defs>
-                  <linearGradient id='colorTotal' x1='0' y1='0' x2='0' y2='1'>
-                    <stop offset='5%' stopColor='#2563eb' stopOpacity={0.8} />
-                    <stop offset='95%' stopColor='#2563eb' stopOpacity={0} />
-                  </linearGradient>
-                </defs>
-                <Tooltip cursor={false} formatter={(value) => `$${value}`} />
-
-                <XAxis
-                  dataKey='name'
-                  strokeWidth={0}
-                  stroke={'#475569'}
-                  tickMargin={6}
-                />
-                <YAxis
-                  dataKey='total'
-                  strokeWidth={0}
-                  stroke={'#475569'}
-                  tickFormatter={(value) => `$${value}`}
-                  tickMargin={6}
-                />
-
-                <Area
-                  type='monotone'
-                  dataKey='total'
-                  stroke='#2563eb'
-                  fillOpacity={1}
-                  fill='url(#colorTotal)'
-                />
-              </AreaChart>
-            </ResponsiveContainer>
-          </div>
-        </div>
-        <div className='card col-span-1 md:col-span-2 lg:col-span-3'>
-          <div className='card-header'>
-            <p className='card-title'>Recent Sales</p>
-          </div>
-          <div className='card-body h-[300px] overflow-auto p-0'>
-            {recentSalesData.map((sale) => (
-              <div
-                key={sale.id}
-                className='flex items-center justify-between gap-x-4 py-2 pr-2'
-              >
-                <div className='flex items-center gap-x-4'>
-                  <img
-                    src={sale.image}
-                    alt={sale.name}
-                    className='size-10 flex-shrink-0 rounded-full object-cover'
-                  />
-                  <div className='flex flex-col gap-y-2'>
-                    <p className='font-medium text-slate-900'>{sale.name}</p>
-                    <p className='text-sm text-slate-600'>{sale.email}</p>
-                  </div>
-                </div>
-                <p className='font-medium text-slate-900'>${sale.total}</p>
+        {statsData.map(({ label, value, icon }, index) => (
+          <div key={index} className='card'>
+            <div className='card-header'>
+              <div className='w-fit rounded-lg bg-blue-500 p-2 text-white transition-colors'>
+                {icon}
               </div>
-            ))}
+              <p className='card-title'>{label}</p>
+            </div>
+            <div className='card-body transition-colors'>
+              <p className='text-3xl font-bold text-slate-900 transition-colors'>
+                {value}
+              </p>
+            </div>
           </div>
-        </div>
-      </div>
-      <div className='card'>
-        <div className='card-header'>
-          <p className='card-title'>Top Orders</p>
-        </div>
-        <div className='card-body p-0'>
-          <div className='relative h-[500px] w-full flex-shrink-0 overflow-auto rounded-none [scrollbar-width:_thin]'>
-            <table className='table'>
-              <thead className='table-header'>
-                <tr className='table-row'>
-                  <th className='table-head'>#</th>
-                  <th className='table-head'>Product</th>
-                  <th className='table-head'>Price</th>
-                  <th className='table-head'>Status</th>
-                  <th className='table-head'>Rating</th>
-                  <th className='table-head'>Actions</th>
-                </tr>
-              </thead>
-              <tbody className='table-body'>
-                {topProducts.map((product) => (
-                  <tr key={product.number} className='table-row'>
-                    <td className='table-cell'>{product.number}</td>
-                    <td className='table-cell'>
-                      <div className='flex w-max gap-x-4'>
-                        <img
-                          src={product.image}
-                          alt={product.name}
-                          className='size-14 rounded-lg object-cover'
-                        />
-                        <div className='flex flex-col'>
-                          <p>{product.name}</p>
-                          <p className='font-normal text-slate-600'>
-                            {product.description}
-                          </p>
-                        </div>
-                      </div>
-                    </td>
-                    <td className='table-cell'>${product.price}</td>
-                    <td className='table-cell'>{product.status}</td>
-                    <td className='table-cell'>
-                      <div className='flex items-center gap-x-2'>
-                        <Star
-                          size={18}
-                          className='fill-yellow-600 stroke-yellow-600'
-                        />
-                        {product.rating}
-                      </div>
-                    </td>
-                    <td className='table-cell'>
-                      <div className='flex items-center gap-x-4'>
-                        <button className='text-blue-500'>
-                          <PencilLine size={20} />
-                        </button>
-                        <button className='text-red-500'>
-                          <Trash size={20} />
-                        </button>
-                      </div>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
